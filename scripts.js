e169f7e64a2313511daf1f2a88da8b469197c63f
EvanaSabatella1989 <eva.sabatella@gmail.com>
2022-05-10 20:55:38 -0300

    adicion de estilos

--- scripts.js
+++ scripts.js
@@ -1,10 +1,11 @@
 const inputTexto = document.querySelector(".input-text");
 const mensaje = document.querySelector(".input-text-area");
-var muneco = document.querySelector(".muneco");
+
 
 function btnencriptar(){
     const textoEncriptado = encriptar(inputTexto.value);
     mensaje.value = textoEncriptado;
+    
     
 }
 
@@ -21,12 +22,15 @@ function btncopiar(){
 function encriptar(StringParaEncriptar){
     let matrizCodigo = [["e", "enter"],["i", "imes"],["a", "ai"],["o", "ober"],["u", "ufat"]]
     StringParaEncriptar = StringParaEncriptar.toLowerCase();
+    
     for(let i=0; i< matrizCodigo.length; i++){
         if(StringParaEncriptar.includes(matrizCodigo[i][0])){
             StringParaEncriptar = StringParaEncriptar.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
 
         }
+       
     }
+    
     return StringParaEncriptar;
 }
 
